bc1702985eeff943d7c508ea4a91f1192d91f30b
Newman Demba <macnoedy@gmail.com>
2023-04-05 11:23:13 -0700

    function that prints a string, followed by a new line.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,14 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-void _puts_recusion(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+
+else
+_putchar('\n');
 }
